185af842dcb25ae1f214643ba2e6aa67016aea9c
botamochi6277 <botamochi6277@icloud.com>
2023-06-11 21:13:27 +0900

    update vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://qiita.com/tat_mae084/items/4051c61926dc8165e80b
 export default defineConfig({
+  base: process.env.GITHUB_PAGES  // この行を追加
+        ? "protopedia-card"            // この行を追加
+        : "./",                     // この行を追加
   plugins: [react()],
 })
