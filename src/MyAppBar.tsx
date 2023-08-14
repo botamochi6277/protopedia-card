1cf125905960576e3080fc1e8b060c398e66a785
botamochi6277 <botamochi6277@icloud.com>
2023-08-14 19:47:24 +0900

    rm unused items

--- src/MyAppBar.tsx
+++ src/MyAppBar.tsx
@@ -1,38 +1,22 @@
 import * as React from 'react'
 
-import axios from 'axios';
-
 import { styled, useTheme } from '@mui/material/styles';
 
 import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import AppBar from "@mui/material/AppBar";
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Menu from '@mui/material/Menu';
-
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-
 
 import Drawer from '@mui/material/Drawer';
 
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+
 // icons
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import UpdateIcon from '@mui/icons-material/Update';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
@@ -44,93 +28,6 @@ import MyForm from './MyForm'
 type MyFormProps = {
   fetchDataHandle: (data: any) => void,
 };
-
-function FetchDataForm(props: MyFormProps) {
-  const [access_token, setAccessToken] = React.useState('');
-  const [token_error, setTokenError] = React.useState(false);
-  const [token_help, setTokenHelp] = React.useState('');
-
-  const [prototype_id, setProtoTypeId] = React.useState('2385');
-  const [prototype_id_error, setPrototypeIdError] = React.useState(false);
-  const [prototype_id_help, setPrototypeIdHelp] = React.useState('');
-
-  const fetchHandle = props.fetchHandle;
-
-  const fetchProjectData = (token: string, prototype_id: string) => {
-    // input validation
-    const token_ref = token.match(/[a-f0-9]{32}/g);
-    if (!token_ref) {
-      setTokenError(true);
-      setTokenHelp('token must be 32 letters')
-      return;
-    } else {
-      setTokenError(false);
-      setTokenHelp('');
-    }
-
-    const id_ref = Number(prototype_id);
-    if (!id_ref) {
-      // id have non-number value
-      setPrototypeIdError(true);
-      setPrototypeIdHelp('ID must be numbers')
-      return;
-    } else {
-      setPrototypeIdError(false);
-      setPrototypeIdHelp('');
-    }
-
-    const url = `https://protopedia.net/api/prototypes.json?token=${token}&prototypeId=${prototype_id}`
-    axios.get(url).then((res) => {
-      console.debug(res);
-      fetchHandle(res.data[0]);
-    }).catch((error) => {
-      console.log(error);
-    })
-  }
-
-  return (
-    <Stack
-      direction={{ xs: 'column', sm: 'row' }}
-      spacing={{ xs: 1, sm: 2, md: 4 }}
-      alignItems="center"
-    >
-      <TextField
-        required
-        id="access-token"
-        label="access token"
-        value={access_token}
-        error={token_error}
-        helperText={token_help}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          setAccessToken(event.target.value)
-        }}
-      />
-
-      <TextField
-        required
-        id="prototype-id"
-        label="prototype id"
-        value={prototype_id}
-        error={prototype_id_error}
-        helperText={prototype_id_help}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          setProtoTypeId(event.target.value)
-        }}
-      />
-
-      <Button
-        variant="contained"
-        startIcon={<UpdateIcon />}
-        onClick={() => { fetchProjectData(access_token, prototype_id) }}>
-        Fetch
-      </Button>
-
-    </Stack>
-  );
-
-}
-
-
 
 
 const drawerWidth = 480;
@@ -153,27 +50,6 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
     marginLeft: 0,
   }),
 }));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-// const AppBar = styled(MuiAppBar, {
-//   shouldForwardProp: (prop) => prop !== 'open',
-// })<AppBarProps>(({ theme, open }) => ({
-//   transition: theme.transitions.create(['margin', 'width'], {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   ...(open && {
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     marginLeft: `${drawerWidth}px`,
-//     transition: theme.transitions.create(['margin', 'width'], {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   }),
-// }));
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -248,7 +124,7 @@ const MyAppBar = (props: MyFormProps) => {
           <ListItem key="fetch data item" disablePadding>
 
             <ListItemIcon>
-              <InboxIcon />
+              <UpdateIcon />
             </ListItemIcon>
             <MyForm fetchDataHandle={props.fetchDataHandle} />
 
