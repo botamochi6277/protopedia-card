fd2f0f46edbc5ac73bf6e822a99d034bc14ed650
botamochi6277 <botamochi6277@icloud.com>
2024-03-04 09:43:05 +0900

    Update background for preview

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,5 +1,5 @@
-import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 const theme = createTheme({
@@ -12,6 +12,10 @@ const theme = createTheme({
     },
     error: {
       main: red.A400,
+    },
+    background: {
+      default: '#e0e0e0',
+      paper: "#ffffff"
     },
   },
 });
